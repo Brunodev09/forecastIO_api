f601ac3446ccd54be62065f48afdb874a31eef4f
Brunodev09 <bruno.mayol.giannotti@gmail.com>
2018-10-27 23:26:46 -0300

    Added Celsius support

--- index.js
+++ index.js
@@ -18,15 +18,16 @@ const argv = yargs
 let encodedAddress = encodeURIComponent(argv.a);
 let geocodeURL = `http://www.mapquestapi.com/geocoding/v1/address?key=2cPAxTCB0oBuMXgF71sO0qGAujCSXD6q&location=${encodedAddress}`;
 
-axios.get(geocodeURL, {units: '[si]'}).then((response) => {
+axios.get(geocodeURL).then((response) => {
     let lat = response.data.results[0].locations[0].latLng.lat;
     let lng = response.data.results[0].locations[0].latLng.lng;
     let tempURL = `https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/${lat},${lng}`;
-    return axios.get(tempURL);
+    console.log(lat,lng);
+    return axios.get(tempURL, {params: {units: 'si'}});
 }).then((response) => {
     let temperature = response.data.currently.temperature;
     let appTemperature = response.data.currently.apparentTemperature;
-    console.log(`The temperature is ${temperature} and the apparent temperature is ${appTemperature}`);
+    console.log(`The temperature is ${temperature}C and the apparent temperature is ${appTemperature}C`);
 }).catch((e) => {
     if (e.code === 'ENOTFOUND') console.log('Cannot connect to servers.');
     else console.log(e);
